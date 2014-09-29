cc5c7365bf657322153975140e92e3dcf101b536
John Heroy <johnheroy@gmail.com>
2014-09-28 17:37:48 -0700

    Add support for city name with spaces

--- whatthetemp.js
+++ whatthetemp.js
@@ -1,6 +1,7 @@
 var args = require('system').args;
-var zipcode = args[args.length - 1];
-var queryURL = 'http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=' + zipcode;
+var cityOrZipcode = args.slice(4).join(' ');
+var locationQuery = encodeURI(cityOrZipcode);
+var queryURL = 'http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=' + locationQuery;
 
 var casper = require('casper').create({
   pageSettings: {
@@ -9,7 +10,7 @@ var casper = require('casper').create({
   }
 });
 
-console.log('Getting the local temp in the ' + zipcode + '...');
+console.log('Getting the local temp in the ' + cityOrZipcode + '...');
 
 casper.start(queryURL, function(){
   var currentTempHTML = this.getHTML('span.wx-data[data-variable="temperature"] span.wx-value');
